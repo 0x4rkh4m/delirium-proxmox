d0ed10d5bceb518ff7fe62bbeb083e167a0af3b1
Noir <alvarodiagoaguilar@gmail.com>
2024-04-06 18:35:12 +0200

    Feat: Improve Guard & Publish 1.1.8

--- libs/proxmox-node-lib/src/common/guard/proxmox.guard.ts
+++ libs/proxmox-node-lib/src/common/guard/proxmox.guard.ts
@@ -1,14 +1,14 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Injectable, CanActivate, UnauthorizedException } from '@nestjs/common';
 import { DeliriumClient } from '../../delirium-client';
 
 @Injectable()
 export class ProxmoxGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const deliriumClient: DeliriumClient = request.deliriumClient;
-    return deliriumClient.isAuthenticated();
+  constructor(private deliriumClient: DeliriumClient) {}
+
+  canActivate(): boolean | Promise<boolean> {
+    if (!this.deliriumClient.isAuthenticated()) {
+      throw new UnauthorizedException();
+    }
+    return true;
   }
 }
